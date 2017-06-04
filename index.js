886720a9d560cc62e07ea2579fed1dd25ef14e84
newdygo <diego.balduino.vip@gmail.com>
2017-06-04 18:54:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 var app = require('express')();
 var bodyParser = require('body-parser')
+var mongo = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/api').post(function(req, res) {
+app.route('/api/click').post(function(req, res) {
     
-    console.log('Starting...');
-    console.log(req.body);
+    mongo.connect('mongodb://admin_conductor:jubekxud2gtcvlal@cdtmongo-shard-00-02-btirt.mongodb.net:27017/mailgun?ssl=true&replicaset=cdtmongo-shard-0&authsource=admin', function(err, db) {
+                       
+        if (!err) {
+
+            var eventt = {
+                city: req.body.city,
+                domain: req.body.domain,
+                recipient: req.body.recipient,
+                event: req.body.event,
+                timestamp: req.body.timestamp,
+                token: req.body.token,
+                signature: req.body.signature
+            };
+
+            db.collection('events').insert(eventt);
+        }
+        
+        db.close();
+    });
 
     res.status(200);
     res.send({message: "OK"});   
