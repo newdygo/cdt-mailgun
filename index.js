4038fa84cd16cba96d5048f0d87519574d5b7bd8
newdygo <diego.balduino.vip@gmail.com>
2017-06-04 18:32:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var app = express();
 app.route('/api').post(function(req, res) {
     
     //console.log('Starting...');
-	
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end();    
+
+    res.status(200);
+    res.send({message: "OK"});   
 });
 
 app.listen(process.env.PORT || 8080);
