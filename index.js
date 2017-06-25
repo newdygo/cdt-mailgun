6820c0010b637fd1f9dde7278a81bd19530e306b
newdygo <diego.balduino.vip@gmail.com>
2017-06-25 16:46:41 -0300

    Update index.js
    Remoção das credenciais do mongodb e correção da inserção

--- index.js
+++ index.js
@@ -7,38 +7,39 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.route('/api/click').post(function(req, res) {
     
-    mongo.connect('mongodb://admin_conductor:TlJYHDVvIufFk3j9@cdtmongo-shard-00-00-btirt.mongodb.net:27017,cdtmongo-shard-00-01-btirt.mongodb.net:27017,cdtmongo-shard-00-02-btirt.mongodb.net:27017/MailGun?ssl=true&replicaSet=CDTMongo-shard-0&authSource=admin', function(err, db) {
+    mongo.connect('mongodb://env.MONGODB_USER:env.MONGODB_PASSWORD@env.MONGODB_SHARD_00,env.MONGODB_SHARD_01,env.MONGODB_SHARD_02/env.MONGODB_DATABASE?ssl=true&replicaSet=CDTMongo-shard-0&authSource=admin', function(err, db) {
                        
         if (!err) {
             
-            var account = {
-                recipient: req.body.recipient,
-                events: [{
-                    $push: {
-                        events: {
-                            domain: req.body.domain,
-                            event: req.body.event,
+            var account = db.collection.findOne({ recipient: req.body.recipient });
+            
+            if (account === null)
+            {
+                account = {
+                    recipient: req.body.recipient,
+                    events: {
+                        domain: req.body.domain,
+                        event: req.body.event,
 
-                            ip: req.body.ip,
-                            device_type: req.body["device-type"],
-                            client_type: req.body["client-type"],
-                            client_name: req.body["client-name"],
-                            user_agent: req.body["user-agent"],
-                            client_os: req.body["client-os"],
+                        ip: req.body.ip,
+                        device_type: req.body["device-type"],
+                        client_type: req.body["client-type"],
+                        client_name: req.body["client-name"],
+                        user_agent: req.body["user-agent"],
+                        client_os: req.body["client-os"],
 
-                            country: req.body.country,
-                            city: req.body.city,
-                            region: req.body.region,
+                        country: req.body.country,
+                        city: req.body.city,
+                        region: req.body.region,
 
-                            message_id: req.body["message-id"],                
-                            tag: req.body.tag,
-                            timestamp: req.body.timestamp,
-                            token: req.body.token,
-                            signature: req.body.signature
-                        }
+                        message_id: req.body["message-id"],                
+                        tag: req.body.tag,
+                        timestamp: req.body.timestamp,
+                        token: req.body.token,
+                        signature: req.body.signature
                     }
-                }]
-            };
+                };
+            }
             
             db.collection('accounts').update({ recipient: req.body.recipient }, account, { upsert: true });
             db.close();
